c79d54870a9adf40b67f5f91c69f3f6fff5fd814
Pavel Pomerantsev <pomerantsevp@gmail.com>
2017-04-15 19:47:22 -0400

    Compare repetitions by flashcardUuid and seq rather than by uuid
    A repetition with a given uuid can be "deleted" by the backend if there's
    a conflict - it actually retains flashcardUuid and seq, but changes
    uuid in case two clients create a repetition for the same flashcard.

--- shared-code/reducers/user-data-reducer.js
+++ shared-code/reducers/user-data-reducer.js
@@ -33,25 +33,27 @@ function getUpdatedState (state, action) {
 
   case 'CREATE_FLASHCARD': {
     const flashcardCreationDay = moment(action.currentTime).diff(moment(constants.SEED_DATE), 'days');
+    const newFlashcard = fromJS({
+      uuid: action.flashcardUuid,
+      frontText: action.frontText,
+      backText: action.backText,
+      createdAt: action.currentTime,
+      updatedAt: action.currentTime
+    });
+    const newRepetition = fromJS({
+      uuid: action.repetitionUuid,
+      flashcardUuid: action.flashcardUuid,
+      seq: 1,
+      plannedDay: flashcardCreationDay + action.diffDays,
+      createdAt: action.currentTime,
+      updatedAt: action.currentTime
+    });
     return state
       .update('flashcards',
-        flashcards => flashcards.push(fromJS({
-          uuid: action.flashcardUuid,
-          frontText: action.frontText,
-          backText: action.backText,
-          createdAt: action.currentTime,
-          updatedAt: action.currentTime
-        }))
+        flashcards => flashcards.push(newFlashcard)
       )
       .update('repetitions',
-        repetitions => repetitions.push(fromJS({
-          uuid: action.repetitionUuid,
-          flashcardUuid: action.flashcardUuid,
-          seq: 1,
-          plannedDay: flashcardCreationDay + action.diffDays,
-          createdAt: action.currentTime,
-          updatedAt: action.currentTime
-        }))
+        repetitions => repetitions.push(newRepetition)
       );
   }
 
@@ -73,15 +75,21 @@ function getUpdatedState (state, action) {
   }
 
   case 'SYNC_DATA': {
+    const flashcardsEqual = (flashcard1, flashcard2) =>
+      flashcard1.get('uuid') === flashcard2.get('uuid');
+    const repetitionsEqual = (repetition1, repetition2) =>
+      repetition1.get('flashcardUuid') === repetition2.get('flashcardUuid') &&
+        repetition1.get('seq') === repetition2.get('seq');
+
     const result = fromJS(action.result);
     const existingFlashcards = result.get('flashcards').filter(receivedFlashcard =>
-        state.get('flashcards').find(flashcard => flashcard.get('uuid') === receivedFlashcard.get('uuid')));
+        state.get('flashcards').find(flashcard => flashcardsEqual(flashcard, receivedFlashcard)));
     const newFlashcards = result.get('flashcards').filter(receivedFlashcard =>
-        !state.get('flashcards').find(flashcard => flashcard.get('uuid') === receivedFlashcard.get('uuid')));
+        !state.get('flashcards').find(flashcard => flashcardsEqual(flashcard, receivedFlashcard)));
     const existingRepetitions = result.get('repetitions').filter(receivedRepetition =>
-        state.get('repetitions').find(repetition => repetition.get('uuid') === receivedRepetition.get('uuid')));
+        state.get('repetitions').find(repetition => repetitionsEqual(repetition, receivedRepetition)));
     const newRepetitions = result.get('repetitions').filter(receivedRepetition =>
-        !state.get('repetitions').find(repetition => repetition.get('uuid') === receivedRepetition.get('uuid')));
+        !state.get('repetitions').find(repetition => repetitionsEqual(repetition, receivedRepetition)));
 
     // TODO: are we sure it's the same request?
     // Handle the case when request didn't go through.
@@ -91,7 +99,7 @@ function getUpdatedState (state, action) {
       .update('flashcards', flashcards => flashcards
         .map(flashcard => {
           const flashcardFromServer = existingFlashcards
-            .find(existingFlashcard => existingFlashcard.get('uuid') === flashcard.get('uuid'));
+            .find(existingFlashcard => flashcardsEqual(existingFlashcard, flashcard));
           return flashcardFromServer ?
             flashcard
               .set('frontText', flashcardFromServer.get('frontText'))
@@ -107,9 +115,10 @@ function getUpdatedState (state, action) {
       .update('repetitions', repetitions => repetitions
         .map(repetition => {
           const repetitionFromServer = existingRepetitions
-            .find(existingRepetition => existingRepetition.get('uuid') === repetition.get('uuid'));
+            .find(existingRepetition => repetitionsEqual(existingRepetition, repetition));
           return repetitionFromServer ?
             repetition
+              .set('uuid', repetitionFromServer.get('uuid'))
               .set('actualDate', repetitionFromServer.get('actualDate'))
               .set('successful', repetitionFromServer.get('successful')) :
             repetition;
