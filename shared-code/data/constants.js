c606c503116c0842f30ad647aba349cfccc3c46d
Pavel Pomerantsev <pomerantsevp@gmail.com>
2017-05-14 12:47:33 -0400

    Extend cookie lifetime to 20 years

--- shared-code/data/constants.js
+++ shared-code/data/constants.js
@@ -2,7 +2,10 @@ export const SEED_DATE = '2012-01-01';
 
 // This affects how soon the app will unexpectedly log out
 // on page load (if not getting the page back from service worker).
-export const COOKIE_EXPIRATION_DAYS = 365;
+// Setting expiration basically to infinity. It's not practical
+// to set an earlier expiration date than that of the token,
+// which currently doesn't expire
+export const COOKIE_EXPIRATION_DAYS = 365 * 20;
 
 export const MAX_REPETITIONS = 3;
 export const MIN_DIFF_DAYS_FIRST_REPETITION = 1;
