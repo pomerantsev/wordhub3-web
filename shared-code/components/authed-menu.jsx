d433a512e8354ce6cf6454a46f981549957dd2cf
Pavel Pomerantsev <pomerantsevp@gmail.com>
2017-05-14 20:34:41 -0400

    Style logged-in menu

--- shared-code/components/authed-menu.jsx
+++ shared-code/components/authed-menu.jsx
@@ -3,6 +3,7 @@ import {getI18n} from '../locales/i18n';
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
+import classNames from 'classnames';
 
 import * as actionCreators from '../data/action-creators';
 
@@ -10,8 +11,30 @@ class AuthedMenu extends React.Component {
 
   constructor () {
     super();
+
+    this.state = {
+      dropdownMenuOpen: false
+    };
+
     this.logout = this.logout.bind(this);
     this.getTodayRepetitionCounts = this.getTodayRepetitionCounts.bind(this);
+    this.onDropdownToggleClick = this.onDropdownToggleClick.bind(this);
+    this.onBodyClick = this.onBodyClick.bind(this);
+    this.toggleRef = this.toggleRef.bind(this);
+  }
+
+  componentDidMount () {
+    this.toggleElement.addEventListener('click', this.onDropdownToggleClick);
+    document.body.addEventListener('click', this.onBodyClick);
+  }
+
+  componentWillUnmount () {
+    this.toggleElement.removeEventListener('click', this.onDropdownToggleClick);
+    document.body.removeEventListener('click', this.onBodyClick);
+  }
+
+  toggleRef (element) {
+    this.toggleElement = element;
   }
 
   logout (event) {
@@ -27,26 +50,38 @@ class AuthedMenu extends React.Component {
     return {total, completed};
   }
 
+  onDropdownToggleClick (event) {
+    event.stopPropagation();
+    this.setState({
+      dropdownMenuOpen: !this.state.dropdownMenuOpen
+    });
+  }
+
+  onBodyClick () {
+    this.setState({
+      dropdownMenuOpen: false
+    });
+  }
+
   render () {
     const repetitionCounts = this.getTodayRepetitionCounts();
     return (
       <div
           className="authed-menu">
-        {this.props.online ?
-          null :
-          <div
-              style={{color: 'red'}}>
-            {getI18n().t('notifications.offline')}
-          </div>
-        }
-        <div>
+        <div
+            className="authed-menu__main">
           <Link
               className="authed-menu__primary-link"
               to="/flashcards/new">
             {getI18n().t('menu.create')}
           </Link>
           <Link
-              className="authed-menu__primary-link"
+              className={classNames({
+                'authed-menu__primary-link': true,
+                'authed-menu__primary-link--to-do': repetitionCounts.completed < repetitionCounts.total,
+                'authed-menu__primary-link--muted': repetitionCounts.completed === repetitionCounts.total
+              })}
+              activeClassName="authed-menu__primary-link--active"
               to="/repetitions">
             <div>
               {getI18n().t('menu.repeat')}
@@ -58,18 +93,60 @@ class AuthedMenu extends React.Component {
               })}
             </div>
           </Link>
-          <Link
-              to="/flashcards">
-            {getI18n().t('menu.allFlashcards')}
-          </Link>
-          <Link
-              to="/stats">
-            {getI18n().t('menu.stats')}
-          </Link>
-          <a
-              onClick={this.logout}>
-            {getI18n().t('menu.logout')}
-          </a>
+          {this.props.online ?
+            null :
+            <span
+                className="authed-menu__primary-link"
+                style={{color: 'red'}}>
+              {getI18n().t('notifications.offline')}
+            </span>
+          }
+          <button
+              ref={this.toggleRef}
+              className="authed-menu__toggle">
+            <div>
+              <span
+                  className="authed-menu__toggle__icon-bar"
+              />
+              <span
+                  className="authed-menu__toggle__icon-bar"
+              />
+              <span
+                  className="authed-menu__toggle__icon-bar"
+              />
+            </div>
+          </button>
+        </div>
+        <div
+            className={classNames(
+              this.state.dropdownMenuOpen ? 'authed-menu__collapsing-menu--open' : 'authed-menu__collapsing-menu--collapsed'
+            )}>
+          <ul
+              className="authed-menu__secondary">
+            <li>
+              <Link
+                  className="authed-menu__secondary-menu-item"
+                  activeClassName="authed-menu__menu-item--active"
+                  to="/flashcards">
+                {getI18n().t('menu.allFlashcards')}
+              </Link>
+            </li>
+            <li>
+              <Link
+                  className="authed-menu__secondary-menu-item"
+                  activeClassName="authed-menu__menu-item--active"
+                  to="/stats">
+                {getI18n().t('menu.stats')}
+              </Link>
+            </li>
+            <li>
+              <a
+                  className="authed-menu__secondary-menu-item"
+                  onClick={this.logout}>
+                {getI18n().t('menu.logout')}
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     );
