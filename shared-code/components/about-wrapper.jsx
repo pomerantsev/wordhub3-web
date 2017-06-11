2c4cee3d1fa89d2f9317c27060d72f7041304a16
Pavel Pomerantsev <pomerantsevp@gmail.com>
2017-06-10 20:43:35 -0400

    Highlight active menu item in about

--- shared-code/components/about-wrapper.jsx
+++ shared-code/components/about-wrapper.jsx
@@ -1,31 +1,51 @@
 import React from 'react';
 import {translate} from 'react-i18next';
 import {Link} from 'react-router';
+import classNames from 'classnames';
 
-function AboutWrapper (props) {
-  const {t} = props;
-  return (
-    <div>
-      <h2>
-        {t('aboutWrapper.heading')}
-      </h2>
-      <ul>
-        <li>
-          <Link
-              to="/intro">
-            {t('aboutWrapper.intro')}
-          </Link>
-        </li>
-        <li>
-          <Link
-              to="/about">
-            {t('aboutWrapper.about')}
-          </Link>
-        </li>
-      </ul>
-      {props.children}
-    </div>
-  );
+import Intro from './intro.jsx';
+import About from './about.jsx';
+
+class AboutWrapper extends React.Component {
+
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
+
+  render () {
+    const routes = this.context.router.routes;
+    const {t} = this.props;
+    return (
+      <div>
+        <h2>
+          {t('aboutWrapper.heading')}
+        </h2>
+        <ul
+            className="about-wrapper__nav">
+          <li
+              className={classNames({
+                'about-wrapper__nav__active-item': !!routes.find(route => route.component === Intro)
+              })}>
+            <Link
+                to="/intro">
+              {t('aboutWrapper.intro')}
+            </Link>
+          </li>
+          <li
+              className={classNames({
+                'about-wrapper__nav__active-item': !!routes.find(route => route.component === About)
+              })}>
+            <Link
+                to="/about">
+              {t('aboutWrapper.about')}
+            </Link>
+          </li>
+        </ul>
+        {this.props.children}
+      </div>
+    );
+  }
+
 }
 
 export default translate()(AboutWrapper);
